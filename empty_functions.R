4bf8ba41dfad3e79604c7824fd675ea9ad4bd40f
Qijun Li <qijunl2@u.washington.edu>
2018-12-13 15:36:54 -0800

    12/13/2018

---
+++ empty_functions.R
@@ -0,0 +1,69 @@
+
+## convert: transform phyloseq object into frequency count data
+
+## alpha_estimate: return an "alpha_estimate" object
+
+## each function first convert a phyloseq object or otu table into a frequency 
+## count table, then give an answer.
+
+## answer include: ()
+
+poisson_model <- function(object, cutoff = 10, print = NULL, plot = NULL) {
+  
+}
+
+geometric_model <- function() {
+  
+}
+
+two_geometric_model <- function() {
+  
+}
+
+three_geometric_model <- function() {
+  
+}
+
+
+##--------------------------------------------------------------
+## Weighted least squre type estimators
+
+
+wlrm_transformed <- function() {
+  
+}
+
+wlrm_untransformed <- function() {
+  
+}
+
+## -----------------------
+## kemp estimator
+
+
+kemp <- function() {
+  
+}
+
+## ----------------------
+## good_turing_richness
+## as chao1, it's a lower bound
+
+good_turing_richness <- function() {
+  
+}
+
+
+## --------------------------
+## chao1
+
+chao1 <- function() {
+  
+}
+
+## --------------------------
+## ace
+
+ace <- function() {
+  
+}
