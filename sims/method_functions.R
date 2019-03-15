8c1ec51f6a6ac2939b9561130e2ce74ada1b91cd
Qijun Li <qijunl2@u.washington.edu>
2019-03-15 10:42:55 -0700

    simulator

--- sims/method_functions.R
+++ sims/method_functions.R
@@ -3,7 +3,7 @@
 
 catchall_best <- new_method("catchall", "Best model with CatchAll",
                         method = function(model, draw) {
-                          bmm <- CatchAll::best_model(draw)
+                          bmm <- CatchAll::catch_best(draw)
                           list(Model = bmm$model,
                                obs = sum(counts > 0),
                                est = bmm$est)
